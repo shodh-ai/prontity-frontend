e52b0561abbce5600e3671ca6fa1cfc5c2f9d189
Harshit Sandilya <harshitsandilya@gmail.com>
2025-06-20 15:31:02 +0530

    renames

--- src/app/layout_test/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function BlobTest() {
-  return (
-    <div>
-      <h1>Layout Test</h1>
-    </div>
-  );
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,5 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  // In a client component, we should redirect to the login page first
-  // This ensures that authentication is checked before showing any content
-  redirect('/loginpage');
-  
-  // This code will never execute due to the redirect
-  return null;
+  redirect("/register");
 }
